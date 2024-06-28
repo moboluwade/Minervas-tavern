64d9504e587392e71d35e2d1a6696763f39fd4b5
moboluwade <folarinboluwade.com>
2024-06-28 02:29:50 +0100

    implement get request  with Open Library API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,23 @@
 import Image from "next/image";
-import { SignInButton } from "./api/(auth)/auth";
-import { auth } from "./auth";
+import { BookSearch } from "./components/BookSearch";
 
 export default async function Home() {
-  const session = await auth()
-  session?.user && console.log(session)
 
 
   return (
     <div className="h-screen w-full">
       <div className="absolute top-0 right-0 bg-gray-600 px-8">
         <div className="flex flex-row gap-2">
-          {session?.user?.image && <Image className="w-8 h-8 rounded-full" width={15} height={15} src={session.user.image} alt="profile" />}
-
-          {session?.user && session?.user.name}
+          
         </div>
       </div>
       <div className="flex flex-col justify-center h-full items-center gap-4">
-        <span className="text-3xl font-semibold tracking-wider ">
+      <BookSearch />
+
+        {/* <span className="text-3xl font-semibold tracking-wider ">
           Minerva
-        </span>
-        <SignInButton />
-
-
+        </span> */}
+        {/* <SignInButton /> */}
       </div>
     </div>
   );
